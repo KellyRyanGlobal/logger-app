ea2b108d52a1ff44718434d8420f8023f834b9ab
Kelly Ryan <kryan@GITI6345.reston.globalinfotek.com>
2021-08-23 19:10:09 +0000

    testing out build

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No poop yet</p>
+                <p className="text-center">No Information yet</p>
             )}
             {items.map(item => (
                 <ItemDisplay
