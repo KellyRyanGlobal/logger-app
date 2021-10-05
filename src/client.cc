50f2d4d5f2794fee29a7987309df8f8915ae5038
Kelly Ryan <kryan@globalinfotek.com>
2021-10-05 18:20:50 +0000

    sends results to client server

--- src/client.cc
+++ src/client.cc
@@ -8,63 +8,95 @@
 #include <string.h>
 
 
-#define PORT 8081
+#define PORT 8050
 
 using namespace std;
 
-static easylogger::Logger HOST("HOST");
+static easylogger::Logger CLIENT("CLIENT");
 
 
-int main(int argc, char const *argv[])
-{
-    int sock = 0, valread;
+class Client_socket{
+
+    fstream myfile;
+    int sock = 0, valread, address_length;
     struct sockaddr_in serv_addr;
-    char hello[] = "Hello from client";
-    std::string mystring;
+    //char hello[] = "Hello from client";
     char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        cout << "\n Socket creation error \n";
-        return -1;
-    }
-   
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-    {
-        cout << "\nInvalid address/ Address not supported \n";
-        return -1;
-    }
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        cout << "\nConnection Failed \n";
-        return -1;
-    }
-    send(sock , hello , strlen(hello) , 0 );
-    cout << "Hello message sent\n";
-    valread = read( sock , buffer , 1024);
-    cout << buffer << endl;;
-    system("./test_app.exe > results.txt");
 
-    // Open the file
-    ifstream myfile;
-    string line; //input variable
-    myfile.open ("results.txt");
-        if (!myfile) // error out if the file could not be opened
-        {
-            cerr << "Error: file could not be opened" << line << endl;
-            exit(1);
+    public:
+        Client_socket () {            
+            create_socket();   
+                
+            serv_addr.sin_family = AF_INET;
+            serv_addr.sin_addr.s_addr = INADDR_ANY; 
+            serv_addr.sin_port = htons(PORT);
+            address_length = sizeof(serv_addr);
+
+            if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) { 
+                cout<<" Invalid address\n";
+            }
+            create_connection();
+            
+            myfile.open(".//input//results.txt", ios::in | ios::binary);
+            if(myfile.is_open()){
+                cout << "[LOG] : File is ready to Transmit.\n";
+            }
+            else{
+                cerr << "[ERROR] : File loading failed, Exititng.\n";
+                exit(EXIT_FAILURE);
+            }
         }
-    while ( getline (myfile,line) ) 
-    {
-        
-        cout << line << endl;
-       // std::cout << mychar;  
-    }
-    myfile.close();
-    cout << "end of file" << endl;
+            // Convert IPv4 and IPv6 addresses from text to binary form
+        void create_socket() {
+            if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+            {
+                cout << "\n Socket creation error \n";                
+                exit(EXIT_FAILURE);
+            }            
+        }
+
+        void create_connection () {
+            if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+            {
+                cout << "\nConnection Failed \n";
+                exit(EXIT_FAILURE);
+            }
+            cout << "connection Successful\n";
+        }
+
+        void send_file() 
+        {
+            std::string mystring ((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
+            int bytes_sent = send(sock , mystring.c_str() , mystring.length() , 0 );
+            cout << "sending :" << mystring << endl;
+            cout << "bytes sent " << bytes_sent << endl;
+            valread = read( sock , buffer , 1024);
+            cout << buffer << endl;;
+            system("./test_app.exe > results.txt");
+
+            // Open the file
+            
+            string line; //input variable
+            myfile.open ("results.txt");
+                if (!myfile) // error out if the file could not be opened
+                {
+                    cerr << "Error: file could not be opened" << line << endl;
+                    exit(1);
+                }
+            while ( getline (myfile,line) ) 
+            {
+                
+                cout << line << endl;
+            // std::cout << mychar;  
+            }
+            myfile.close();
+            cout << "end of file" << endl;
+        }
+};
+
+int main(){
+    Client_socket C;
+    LOG_INFO(CLIENT, "obtained information");
+    C.send_file();
     return 0;
 }
--- src/server.cc
+++ src/server.cc
@@ -9,66 +9,100 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#define PORT 8081
+#include <fstream>
 
-int main(int argc, char const *argv[])
-{
-    int server_fd, new_socket, valread;
+#define PORT 8050
+
+class Server_socket{
+
+    fstream myfile;
+    int server_fd, valread, opt = 1, new_socket, address_length;
     struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+    int addrlen;
     char buffer[1024] = {0};
-    char hello[] = "Hello from server";
-       
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-       
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-       
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read( new_socket , buffer, 1024);
-    cout <<  buffer << endl;
-    send(new_socket , hello , strlen(hello) , 0 );
-    cout << "Hello message sent\n";
-     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read( new_socket , buffer, 1024);
-
-
+    // char hello[] = "Hello from server";
     
+    public:
+        Server_socket () {
+            create_socket();
+                
+            address.sin_family = AF_INET;
+            address.sin_addr.s_addr = INADDR_ANY;
+            address.sin_port = htons( PORT );            
+            address_length = sizeof(address);
+
+            bind_socket();
+            set_listen_set();
+            accept_connection();
+
+            myfile.open(".//output//output.out", ios::out | ios::trunc | ios::binary);
+            if(myfile.is_open()) {
+                cout<<"[LOG] : File Creted.\n";
+            }
+            else{
+                cerr <<" File creation failed, Exititng.\n";
+                exit(EXIT_FAILURE);
+            }
+        }
+        // Creating socket file descriptor
+        void create_socket() {
+            if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+            {
+                cerr << "socket failed\n";
+                exit(EXIT_FAILURE);
+            }
+            cout << " Socket Created\n";
+        }
+        // Forcefully attaching socket to the port 8080
+        // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+        //                                             &opt, sizeof(opt)))
+        // {
+        //     perror("setsockopt");
+        //     exit(EXIT_FAILURE);
+        // }
+        
+        // Forcefully attaching socket to the port 8080
+        void bind_socket() {
+            if (bind(server_fd, (struct sockaddr *)&address, 
+                                        sizeof(address))<0)
+            {
+                cerr << "Bind failed\n";
+                exit(EXIT_FAILURE);
+            }
+            cout << " Bind Successful.\n";
+        }
+
+        void set_listen_set() {
+            if (listen(server_fd, 3) < 0)
+            {
+                cerr <<  "listen\n";
+                exit(EXIT_FAILURE);
+            }
+            cout << " Socket Listening\n";
+        }
+
+        void accept_connection() {
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                            (socklen_t*)&addrlen))<0)
+            {
+                cerr << "accept";
+                exit(EXIT_FAILURE);
+            }
+            cout << "Connected to client\n";
+        }
+
+        void receive_file(){
+            char buffer[1024] = {};
+            int valread = read( new_socket , buffer, 1024);
+            cout <<"Data received "<< valread <<" bytes\n";
+            cout <<"Saving data to file.\n";            
+            myfile << buffer;
+            cout <<" File Saved.\n";
+        }
+};
+
+int main(){
+    Server_socket S;
+    S.receive_file();
     return 0;
 }
