f05caf121a72ef18d7f53af2841b108717e90899
Kelly Ryan <kryan@globalinfotek.com>
2021-10-14 20:29:34 +0000

    removing dpeendency

--- build.sh
+++ build.sh
@@ -2,18 +2,12 @@
 
 main() {
 
-    if [[ "$1" == "ubuntu" ]]; then
+    if [[ -z $1  ]]; then
         make -C src
         mkdir -p input output        
         sleep 5        
         ./server.exe &
         ./test_client.exe
-    elif [[ "$1" == "alpine" ]]; then
-        make -C src
-        mkdir -p input output
-        sleep 5
-        ./server.exe &
-        ./test_client.exe  
     else
         echo " Host was not passed as a param (see config)"
         return 1
