f273739f3857e49b4e94abd07a4bf5aa3c55ccbe
Kelly Ryan <kryan@globalinfotek.com>
2021-10-14 18:14:12 +0000

    fixed typo

--- build.sh
+++ build.sh
@@ -7,18 +7,20 @@ main() {
         mkdir -p src/input src/output
         src/server.exe &
         src/test_client.exe
+        return 0
     }
     if [[ $1 == "alpine "]]; then
     {
         make -C src
         mkdir input output
         ./server.exe &
-        ./test_client.exe
+        ./test_client.exe    
+        return 0
     }
     else
         echo " Host was not passed as a param (see config)"
         return 1
-    fireturn -
+    fi
 }
 
 main "$@"
