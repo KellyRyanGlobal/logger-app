f3783cc97936344e35741060158fe4ea0604b7e6
Kelly Ryan <kryan@globalinfotek.com>
2021-10-14 18:50:26 +0000

    trying to build

--- build.sh
+++ build.sh
@@ -4,20 +4,21 @@ main() {
 
     if [[ "$1" == "ubuntu" ]]; then
         make -C src
-        mkdir -p src/input src/output
-        src/server.exe &
-        src/test_client.exe
-        return 0
-    elif [[ "$1" == "alpine " ]]; then
-        make -C src
-        mkdir input output
+        mkdir -p input output        
+        sleep 5        
         ./server.exe &
-        ./test_client.exe    
-        return 0
+        ./test_client.exe
+    elif [[ "$1" == "alpine" ]]; then
+        make -C src
+        mkdir -p input output
+        sleep 5
+        ./server.exe &
+        ./test_client.exe  
     else
         echo " Host was not passed as a param (see config)"
         return 1
     fi
+    return 0
 
 }
 
