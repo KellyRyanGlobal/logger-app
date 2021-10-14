02923f0c06b9803a6789267e4d1befa9c89800cf
Kelly Ryan <kryan@globalinfotek.com>
2021-10-14 18:15:42 +0000

    fixed typo

--- build.sh
+++ build.sh
@@ -2,21 +2,17 @@
 
 main() {
     if [[ $1 == "ubuntu" ]]; then
-    {
         make -C src
         mkdir -p src/input src/output
         src/server.exe &
         src/test_client.exe
         return 0
-    }
     if [[ $1 == "alpine "]]; then
-    {
         make -C src
         mkdir input output
         ./server.exe &
         ./test_client.exe    
         return 0
-    }
     else
         echo " Host was not passed as a param (see config)"
         return 1
