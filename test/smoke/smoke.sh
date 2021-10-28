918029d4472a881ec04fc0de41668e13b760cfe4
AnnMillikin <ann22301@gmail.com>
2021-10-28 11:07:57 +0000

    move Internal Field Separator logic to smoke

--- test/smoke/smoke.sh
+++ test/smoke/smoke.sh
@@ -14,6 +14,8 @@ main() {
     check_files "test_client.exe"
     check_files "server.exe"
 
+    echo "reading in $1 "
+    IFS=$'\r\n' command eval 'results=($(cat $1))'
     echo "Check if results not null"
     not_empty $results
 
